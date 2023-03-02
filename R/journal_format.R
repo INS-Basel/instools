78d4584ec0077cd3e2863ae7c0a959fd296b9ff0
Catherine Blatter <catherine.blatter@unibas.ch>
2023-03-02 08:18:56 +0100

    implement journal_format()
    addresses #15

--- R/journal_format.R
+++ R/journal_format.R
@@ -1,13 +1,21 @@
 #' Format numbers according to journal requirements
 #'
+#' @details some journal require to format big numbers only from the
+#' fifth digit (instead of the fourth) - this functions fixes this
+#'
 #' @param x an integer
 #' @param n_break fix number where the formatting sets the break
-#' @param ... additional arguments passed to base::format()
+#' @param big.mark defaults to ",", passed to \code{base::format(big.mark)}
+#' @param ... additional arguments passed to \code{base::format()}
 #'
 #' @return an formatted character string
-#' @keywords internal
-journal_format <- function(x, n_break = 4, ...){
+#' @export
+#'
+#' @examples
+#' vec <- c(1L, 20L, 300L, 4000L, 50000L)
+#' journal_format(vec, big.mark = ",")
+journal_format <- function(x, n_break = 4, big.mark = ",", ...){
   dplyr::case_when(
-    base::nchar(x) > n_break ~ base::format(x, ...),
+    base::nchar(x) > n_break ~ base::format(x, big.mark = big.mark, ...),
     TRUE ~ base::format(x))
 }
---
+++ tests/testthat/test-journal-format.R
@@ -0,0 +1,22 @@
+test_that("formatting works without bigmark", {
+  vec <- c(1L, 20L, 300L, 4000L, 50000L)
+  expect_equal(
+    journal_format(vec),
+    c("    1",
+      "   20",
+      "  300",
+      " 4000",
+      "50,000"))
+})
+
+
+test_that("formatting works with (silly!!) bigmark", {
+  vec <- c(1L, 20L, 300L, 4000L, 50000L)
+  expect_equal(
+    journal_format(vec, big.mark = ";;"),
+    c("    1",
+      "   20",
+      "  300",
+      " 4000",
+      "50;;000"))
+})
